c6a0ca43fb6d5c9b8c89151d78597c053458c9aa
DanGuttt <123544055+DanGuttt@users.noreply.github.com>
2025-12-05 01:22:01 +0100

    Create Rose.h

---
+++ Rose.h
@@ -0,0 +1,12 @@
+#ifndef FIRST_H
+#define FIRST_H
+
+// Prototypes des fonctions utilisées dans first.c
+
+int n1Neg(int n1, int n2);
+int n2Neg(int n1, int n2);
+int n1Null(int n1, int n2);
+int n2Null(int n1, int n2);
+int first(int n1, int n2);
+
+#endif
